ab532882a904f4b8b993def51702fdb50cb123bc
이의정 <stxtory@gmail.com>
2024-12-01 00:13:57 +0900

    11.1 스프링 클라우드 OpenFeign으로 REST 엔드포인트 호출

---
+++ openFeign/src/main/java/com/example/demo/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Payment;
+import com.example.demo.proxy.PaymentsProxy;
+
+@RestController
+public class PaymentController {
+	private final PaymentsProxy paymentsProxy;
+	
+	public PaymentController(PaymentsProxy paymentsProxy) {
+		this.paymentsProxy = paymentsProxy;
+	}
+	
+	@PostMapping("/payment")
+	public Payment createPayment(
+			@RequestBody Payment payment) {
+		String requestId = UUID.randomUUID().toString();
+		return paymentsProxy.createPayment(requestId, payment);
+	}
+}
